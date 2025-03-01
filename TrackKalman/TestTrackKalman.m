b69111071e235f6fe6117ed91b93d4929512e648
Young Jin Son <0jin@Youngui-Mac-Studio.local>
2025-03-02 01:34:35 +0900

    위치-속도, 물체 추적

---
+++ TrackKalman/TestTrackKalman.m
@@ -0,0 +1,31 @@
+clear all
+
+NoOfImg = 24;
+
+Xmsaved = zeros(2, NoOfImg);
+Xhsaved = zeros(2, NoOfImg);
+
+for k = 1:NoOfImg
+  [xm, ym] = GetBallPos(k);
+  [xh, yh] = TrackKalman(xm, ym);
+      
+  hold on
+  plot(xm, ym, 'r*')
+  plot(xh, yh, 'bs')
+  
+  pause(1)
+  
+  Xmsaved(:, k) = [xm ym]';
+  Xhsaved(:, k) = [xh yh]';
+
+end
+
+
+figure
+hold on
+plot(Xmsaved(1,:), Xmsaved(2,:), '*','DisplayName','Measured')
+plot(Xhsaved(1,:), Xhsaved(2,:), 's', 'DisplayName','Kalman Filter')
+xlabel('Horizontal [pixel]')
+ylabel('Vertical [pixel]')
+
+set(gca, 'YDir', 'reverse');
